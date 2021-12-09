98dce5df1148010e3d439284ae163013c48857e8
Thomas Madden <tmadden@gmail.com>
2021-12-08 22:25:34 -0500

    Add neighbors

--- utilities.py
+++ utilities.py
@@ -84,3 +84,45 @@ palette = [light_gorgeous, gorgeous, snowy, pretty, good_purple]
 
 # prominent_lights = ['5l4', '2r8', '4h4']
 prominent_lights = [14, 16, 9]
+
+from math import sqrt
+
+
+def light_distance(a, b):
+    dx = a.p[0] - b.p[0]
+    dy = a.p[1] - b.p[1]
+    return sqrt(dx * dx + dy * dy)
+
+
+def compute_neighbors(lights):
+    import numpy as np
+
+    points = np.array([light.p for light in lights])
+    from scipy.spatial import Delaunay
+    tris = Delaunay(points)
+
+    # import matplotlib.pyplot as plt
+    # plt.triplot(points[:, 0], points[:, 1], tris.simplices)
+    # plt.plot(points[:, 0], points[:, 1], 'o')
+    # for index, light in enumerate(lights):
+    #     plt.text(light.p[0], light.p[1], str(index))
+    # plt.show()
+
+    def add_neighbors(i, j):
+        neighbors[i].add(j)
+        neighbors[j].add(i)
+
+    neighbors = [set() for light in lights]
+    for tri in tris.simplices:
+        add_neighbors(tri[0], tri[1])
+        add_neighbors(tri[1], tri[2])
+        add_neighbors(tri[2], tri[0])
+    return neighbors
+
+
+neighbors = [{3, 5, 8, 9, 14}, {8, 11, 12}, {6, 9, 10, 13, 14, 15},
+             {0, 10, 14, 15}, {5, 6, 7, 8, 11}, {0, 4, 6, 8, 9},
+             {2, 4, 5, 7, 9, 13}, {4, 6, 11, 13, 16, 17}, {0, 1, 4, 5, 11},
+             {0, 2, 5, 6, 14}, {2, 3, 13, 15, 16}, {1, 4, 7, 8, 12, 17},
+             {1, 11, 17}, {2, 6, 7, 10, 16}, {0, 2, 3, 9, 15}, {3, 2, 10, 14},
+             {17, 10, 13, 7}, {16, 11, 12, 7}]
